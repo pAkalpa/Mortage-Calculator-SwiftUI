18bd7ee20b07e72a59aaf4b8c39f543047b42113
pAkalpa <pasinduakalpa1998@gmail.com>
2023-03-13 08:16:12 +0530

    Initial Commit

---
+++ Mortage Calculator/Mortage_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mortage_CalculatorApp.swift
+//  Mortage Calculator
+//
+//  Created by Buddhika Kalupahana on 2023-03-13.
+//
+
+import SwiftUI
+
+@main
+struct Mortage_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
